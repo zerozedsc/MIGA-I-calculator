da8a790ca1f674a8971ede8fdc8febca857292c0
Helmi <zerozed_hm1@hotmail.com>
2026-01-28 19:45:57 +0900

    fix problem

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,24 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './index.html',
-    './index.tsx',
-    './App.tsx',
-    './components/**/*.{ts,tsx}',
-    './utils/**/*.{ts,tsx}',
-  ],
-  theme: {
-    extend: {
-      colors: {
-        maybank: {
-          yellow: '#FFD100',
-          dark: '#D4AF37',
+    content: [
+        './index.html',
+        './index.tsx',
+        './App.tsx',
+        './components/**/*.{ts,tsx}',
+        './utils/**/*.{ts,tsx}',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                maybank: {
+                    yellow: '#FFD100',
+                    dark: '#D4AF37',
+                },
+            },
+            fontFamily: {
+                sans: ['Inter', 'sans-serif'],
+            },
         },
-      },
-      fontFamily: {
-        sans: ['Inter', 'sans-serif'],
-      },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
